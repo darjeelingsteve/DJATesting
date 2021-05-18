ffa53fa96222d1369ae7e52ae9fef7671441e47e
Stephen Anthony <steve@darjeelingapps.com>
2021-05-18 19:29:01 +0100

    Add UIControl+Testing

---
+++ DJATesting/UIControl+Testing.swift
@@ -0,0 +1,17 @@
+//
+//  UIControl+Testing.swift
+//  DJATesting
+//
+//  Created by Stephen Anthony on 18/05/2021.
+//
+
+import UIKit
+
+public extension UIControl {
+    
+    /// Sends the receiver's actions for the `.primaryActionTriggered` control
+    /// event.
+    func simulatePrimaryActionTriggered() {
+        sendActions(for: .primaryActionTriggered)
+    }
+}
