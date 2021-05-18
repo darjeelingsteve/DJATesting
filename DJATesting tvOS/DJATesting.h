bb852599fca06084681241fa48ba710e1666eefc
Stephen Anthony <steve@darjeelingapps.com>
2021-05-18 20:08:01 +0100

    Add DJATesting tvOS framework

---
+++ DJATesting tvOS/DJATesting.h
@@ -0,0 +1,18 @@
+//
+//  DJATesting.h
+//  DJATesting tvOS
+//
+//  Created by Stephen Anthony on 18/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DJATesting.
+FOUNDATION_EXPORT double DJATestingVersionNumber;
+
+//! Project version string for DJATesting.
+FOUNDATION_EXPORT const unsigned char DJATestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DJATesting/PublicHeader.h>
+
+#import <DJATesting/UIGestureRecognizer+Testing.h>
