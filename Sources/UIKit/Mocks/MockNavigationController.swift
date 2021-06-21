4bfc4cfdbd2dca9507d5876027e359f37e3c30e6
Stephen Anthony <steve@darjeelingapps.com>
2021-06-21 13:55:34 +0100

    Add -[MockNavigationController overriddenPresentedViewController]

--- Sources/UIKit/Mocks/MockNavigationController.swift
+++ Sources/UIKit/Mocks/MockNavigationController.swift
@@ -52,6 +52,14 @@ public class MockNavigationController: UINavigationController {
         super.dismiss(animated: flag, completion: completion)
     }
     
+    /// The view controller that the receiver will return from
+    /// `presentedViewController`, if any.
+    public var overriddenPresentedViewController: UIViewController?
+    
+    public override var presentedViewController: UIViewController? {
+        return overriddenPresentedViewController ?? super.presentedViewController
+    }
+    
     /// The mock transition coordinator returned by the receiver's
     /// `transitionCoordinator` property.
     public let mockTransitionCoordinator = MockTransitionCoordinator()
