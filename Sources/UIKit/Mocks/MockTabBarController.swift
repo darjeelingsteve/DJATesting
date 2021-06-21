0dcdc94b6272fee8fff3496408e7abb37aec2de0
Stephen Anthony <steve@darjeelingapps.com>
2021-06-21 12:47:33 +0100

    Add MockTabBarController

---
+++ Sources/UIKit/Mocks/MockTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  MockTabBarController.swift
+//  DJATesting
+//
+//  Created by Stephen Anthony on 21/06/2021.
+//
+
+import UIKit
+
+public class MockTabBarController: UITabBarController {
+    private(set) public var receivedViewControllers: [UIViewController]?
+    private(set) public var receivedViewControllersAnimationFlag: Bool?
+    public override func setViewControllers(_ viewControllers: [UIViewController]?, animated: Bool) {
+        receivedViewControllers = viewControllers
+        receivedViewControllersAnimationFlag = animated
+        super.setViewControllers(viewControllers, animated: animated)
+    }
+    
+    private(set) public var receivedViewControllerForPresentation: UIViewController?
+    public override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
+        receivedViewControllerForPresentation = viewControllerToPresent
+        super.present(viewControllerToPresent, animated: flag, completion: completion)
+    }
+}
