376833d00be6b0bce400883b36b8e71e53192658
Stephen Anthony <steve@darjeelingapps.com>
2021-06-25 12:20:07 +0100

    Add UIBarButtonItem+Testing

---
+++ Sources/UIKit/UIBarButtonItem+Testing.swift
@@ -0,0 +1,19 @@
+//
+//  UIBarButtonItem+Testing.swift
+//  DJATesting
+//
+//  Created by Stephen Anthony on 25/06/2021.
+//
+
+import UIKit
+
+public extension UIBarButtonItem {
+    func simulateTap() {
+        guard let action = action else { return }
+        if action.description.contains(":") {
+            _ = target?.perform(action, with: self)
+        } else {
+            _ = target?.perform(action)
+        }
+    }
+}
