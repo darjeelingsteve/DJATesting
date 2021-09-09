6600a69208981efecdc32552f52236842249655a
Stephen Anthony <steve@darjeelingapps.com>
2021-09-09 12:11:04 +0100

    Add -[UIKeyCommand simulateCommandInvocationOnResponder:]

---
+++ Sources/UIKit/UIKeyCommand+Testing.swift
@@ -0,0 +1,23 @@
+//
+//  UIKeyCommand+Testing.swift
+//  DJATesting
+//
+//  Created by Stephen Anthony on 09/09/2021.
+//
+
+import UIKit
+
+public extension UIKeyCommand {
+    
+    /// Simulates the user invoking the receiver.
+    /// - Parameter responder: The responder to which the receiver's `action`
+    /// should be sent.
+    func simulateCommandInvocation(on responder: UIResponder) {
+        guard let action = action else { return }
+        if action.description.contains(":") {
+            _ = responder.perform(action, with: self)
+        } else {
+            _ = responder.perform(action)
+        }
+    }
+}
