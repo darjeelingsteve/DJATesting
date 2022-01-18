f0d8eafaf62138dfe4514303c2f822c8fc6a468e
Stephen Anthony <steve@darjeelingapps.com>
2022-01-18 15:31:23 +0000

    Add documentation for -[UIBarButtonItem simulateTap]

--- Sources/DJATesting/UIKit/UIBarButtonItem+Testing.swift
+++ Sources/DJATesting/UIKit/UIBarButtonItem+Testing.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 public extension UIBarButtonItem {
+    
+    /// Simulates the user tapping on the receiver.
     func simulateTap() {
         guard let action = action else { return }
         if action.description.contains(":") {
