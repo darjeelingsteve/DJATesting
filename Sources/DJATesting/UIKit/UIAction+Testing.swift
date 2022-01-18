22d2c12c06558fbc980d4b4e29c098899f24abdb
Stephen Anthony <steve@darjeelingapps.com>
2022-01-18 14:36:06 +0000

    Use consistent invocation naming

--- Sources/DJATesting/UIKit/UIAction+Testing.swift
+++ Sources/DJATesting/UIKit/UIAction+Testing.swift
@@ -12,7 +12,7 @@ public extension UIAction {
     private typealias ActionHandler = @convention(block) (UIAction) -> Void
     
     /// Invokes the `handler` closure that the reciever was initialised with.
-    func triggerHandler() {
+    func invokeHandler() {
         // Using key from https://developer.limneos.net/?ios=14.4&framework=UIKitCore.framework&header=UIAction.h
         guard let block = value(forKey: "handler") else { return }
         let blockPointer = UnsafeRawPointer(Unmanaged<AnyObject>.passUnretained(block as AnyObject).toOpaque())
--- Sources/DJATesting/UIKit/UIAlertAction+Testing.swift
+++ Sources/DJATesting/UIKit/UIAlertAction+Testing.swift
@@ -11,7 +11,7 @@ public extension UIAlertAction {
     private typealias ActionHandler = @convention(block) (UIAlertAction) -> Void
 
     /// Invokes the `handler` closure that the reciever was initialised with.
-    func triggerHandler() {
+    func invokeHandler() {
         // Using key from https://developer.limneos.net/?ios=14.4&framework=UIKitCore.framework&header=UIAlertAction.h
         guard let block = value(forKey: "handler") else { return }
         let blockPointer = UnsafeRawPointer(Unmanaged<AnyObject>.passUnretained(block as AnyObject).toOpaque())
