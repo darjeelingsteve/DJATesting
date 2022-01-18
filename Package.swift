ab5d9c586eedd7e3d5627c2347bd6bfd310863b9
Stephen Anthony <steve@darjeelingapps.com>
2022-01-18 09:08:56 +0000

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DJATesting",
+    platforms: [
+        .iOS(.v10),
+        .tvOS(.v10)
+    ],
+    products: [
+        .library(name: "DJATesting", type: .dynamic, targets: ["DJATesting"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "DJATesting_ObjC", path: "Sources/DJATesting/ObjC"),
+        .target(name: "DJATesting", dependencies: ["DJATesting_ObjC"], exclude: ["ObjC"])
+    ],
+    swiftLanguageVersions: [.v5]
+)
