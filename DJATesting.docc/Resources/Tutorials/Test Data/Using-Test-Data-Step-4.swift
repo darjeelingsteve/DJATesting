c4ad3f23225c7a726d8f7b02d63c8383a451230a
Stephen Anthony <steve@darjeelingapps.com>
2022-01-26 18:05:21 +0000

    Add test data tutorial

---
+++ DJATesting.docc/Resources/Tutorials/Test Data/Using-Test-Data-Step-4.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import MyApp
+
+final class UserListDataSourceTests: XCTest {
+    private var userListDataSource: UserListDataSource!
+    private var collectionView: UICollectionView!
+    
+    override func setUp() {
+        super.setUp()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    private func givenAUserListDataSource() {
+        userListDataSource = UserListDataSource(collectionView: collectionView, cellProvider: { _, _, _ -> UICollectionViewCell? in
+            return nil
+        })
+    }
+    
+    private func whenUsersAreAssignedToTheDataSource() {
+        let users = try! [User].from(fileName: "DummyUsers", inBundle: Bundle(for: UserListDataSourceTests.self))
+        userListDataSource.users = users
+    }
+}
