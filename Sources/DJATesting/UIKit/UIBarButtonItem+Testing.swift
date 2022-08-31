b0f35bd10b676a9c6d20207edd470f7baefb7e0c
Stephen Anthony <steve@darjeelingapps.com>
2022-08-31 14:56:59 +0100

    Add -[UIBarButtonItem systemItem]

--- Sources/DJATesting/UIKit/UIBarButtonItem+Testing.swift
+++ Sources/DJATesting/UIKit/UIBarButtonItem+Testing.swift
@@ -11,6 +11,13 @@ import UIKit
 
 public extension UIBarButtonItem {
     
+    /// The `UIBarButtonItem.SystemItem` that the reciever was initialised with,
+    /// if any.
+    var systemItem: UIBarButtonItem.SystemItem? {
+        guard let rawValue = value(forKey: "systemItem") as? Int else { return nil }
+        return UIBarButtonItem.SystemItem(rawValue: rawValue)
+    }
+    
     /// Simulates the user tapping on the receiver.
     func simulateTap() {
         guard let action = action else { return }
