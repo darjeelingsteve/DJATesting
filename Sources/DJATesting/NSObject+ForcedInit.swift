1c58248f501bec417be5215a5216cf0a74b83658
Stephen Anthony <steve@darjeelingapps.com>
2022-09-21 11:26:25 +0100

    Add +[NSObject forcedInit]

---
+++ Sources/DJATesting/NSObject+ForcedInit.swift
@@ -0,0 +1,18 @@
+//
+//  NSObject+ForcedInit.swift
+//  DJATesting
+//
+//  Created by Stephen Anthony on 21/09/2022.
+//
+
+import Foundation
+
+private extension NSObject {
+    
+    /// A workaround for classes whose `init` methods are marked as deprecated
+    /// or are made inaccessible.
+    /// - Returns: A newly initialised object of the target class.
+    static func forcedInit() -> Self {
+        Self.self.init()
+    }
+}
